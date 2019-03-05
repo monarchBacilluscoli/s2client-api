ea0918a8a0da02d57d57b341d129c83b13424573
永峰 刘 <20131002682@cug.edu.com>
2019-03-05 22:34:10 +0800

    added wall force in PF_bot

--- examples/my_bots/potential_field/potential_field.h
+++ examples/my_bots/potential_field/potential_field.h
@@ -12,6 +12,7 @@ namespace sc2 {
 
         Vector2D force_enemy_to_unit(const Unit* source, const Unit* target);
         Vector2D force_ally_to_unit(const Unit* source, const Unit* target);
+        Vector2D force_wall_to_unit(const Unit* target);
 
         Vector2D force_to_unit(const Units& sources, const Unit* u);
         float calculate_enemy_zero_field_dis(const Unit* source, const Unit* target);
@@ -31,15 +32,17 @@ namespace sc2 {
         void display_force_direction(const Unit* u, const Vector2D& force,DebugInterface* debug);
         void display_field_boundary(const Unit* selected_u, const Units& source_us, DebugInterface* debug);
         void display_fire_range(const Units& us, DebugInterface* debug);
+        void display_playable_area(const GameInfo& gi, DebugInterface* debug);
 
-        const float zero_potential_energy_ratio = 0.8f;
-        const float zero_ally_field_ratio = 0.6f;
+        const float m_zero_potential_energy_ratio = 0.8f;
+        const float m_zero_ally_field_ratio = 0.6f;
+        const float m_wall_force_factor = 10; // must be big enough
 
         Units m_alive_self_units;
         Units m_alive_enemy_units;
         Units m_all_alive_units;
         UnitTypes m_unit_types;
-
+        GameInfo m_game_info;
     };
 }
 
