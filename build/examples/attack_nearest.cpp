28b7272d38ed739c86e6f742fe79d329d6c40223
永峰 刘 <20131002682@cug.edu.com>
2019-03-12 09:19:04 +0800

    Since the built-in bot of StarCraft II have a obvious shortage that it units may walk between two enemy unit repeatedly so I added my own rule-based bot base class and a attack nearest bot derived class. But as I have seen, the results are the same with the built-in bot. It is normal to see the distances from a self unit to enemy  units changes all the time and any distance can be the shortest at any point of time after all.

---
+++ build/examples/attack_nearest.cpp
@@ -0,0 +1,18 @@
+#include "attack_nearest.h"
+
+namespace sc2 {
+	const Unit* sc2::rule_based_bot::select_nearest_unit_from_point(const Point2D& p, const Units& us) {
+		float min_distance = FLT_MAX;
+		const Unit* selected_unit = nullptr;
+		float dis;
+		for (const auto u : us) {
+			dis = Distance2D(p, u->pos);
+			if (dis < min_distance) {
+				selected_unit = u;
+				min_distance = dis;
+			}
+		}
+		return selected_unit;
+	}
+}
+
