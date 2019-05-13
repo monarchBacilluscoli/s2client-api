45be9e780a56350d9e395694bffe319577e7629e
永峰 刘 <20131002682@cug.edu.com>
2019-05-13 09:39:00 +0800

    1. Fixed a mistake in feature_layers example 2. Added some comments in it to help me understand the mechanism

--- examples/my_bots/tests/remote_draw.h
+++ examples/my_bots/tests/remote_draw.h
@@ -8,47 +8,76 @@
 #include "sc2utils/sc2_manage_process.h"
 
 #include<iostream>
-//#include "../my_bots/rule_based_bots/rule_based_bot.h"
+#include "../my_bots/rule_based_bots/rule_based_bot.h"
 #include "sc2renderer/sc2_renderer.h"
 
+namespace sc2 {
+    class remote_draw : public Agent,public rule_based_bot
+    {
+    public:
+        remote_draw() = default;
+        ~remote_draw() = default;
+
+        virtual void OnGameStart() final {
+            //Initialize data
+            m_alive_enemy_units = Agent::Observation()->GetUnits(Unit::Alliance::Enemy);
+
+            sc2::renderer::Initialize("Feature layers", 50, 50, 2 * kDrawSize, 2 * kDrawSize);
+        }
+
+        virtual void OnStep() final {
+            //Update data
+            m_alive_enemy_units = Agent::Observation()->GetUnits(Unit::Alliance::Enemy);
+
+            const SC2APIProtocol::Observation* observation = Agent::Observation()->GetRawObservation();
+
+            const SC2APIProtocol::FeatureLayers& m = observation->feature_layer_data().renders();
+            DrawFeatureLayerUnits8BPP(m.unit_density(), 0, 0);
+            DrawFeatureLayer1BPP(m.selected(), kDrawSize, 0);
+
+            const SC2APIProtocol::FeatureLayersMinimap& mi = observation->feature_layer_data().minimap_renders();
+            DrawFeatureLayerHeightMap8BPP(mi.height_map(), 0, kDrawSize);
+            DrawFeatureLayer1BPP(mi.camera(), kDrawSize, kDrawSize);
+
+            sc2::renderer::Render();
+        }
+
+        virtual void OnUnitIdle(const Unit* unit) {
+            const Unit* target = select_nearest_unit_from_point(unit->pos, m_alive_enemy_units);
+            Agent::Actions()->UnitCommand(unit, ABILITY_ID::ATTACK, target);
+        }
 
 
-class remote_draw : public sc2::Agent
-{
-public:
-    remote_draw() = default;
-    ~remote_draw() = default;
+    private:
+        void DrawFeatureLayer1BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
+            assert(image_data.bits_per_pixel() == 1);
+            int width = image_data.size().x();
+            int height = image_data.size().y();
+            sc2::renderer::Matrix1BPP(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
+        }
 
-    virtual void OnGameStart() final {
-        sc2::renderer::Initialize("Feature layers", 50, 50, 2 * kDrawSize, 2 * kDrawSize);
-    }
+        void DrawFeatureLayerUnits8BPP(const SC2APIProtocol::ImageData & image_data, int off_x, int off_y) {
+            assert(image_data.bits_per_pixel() == 8);
+            int width = image_data.size().x();
+            int height = image_data.size().y();
+            sc2::renderer::Matrix8BPPPlayers(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
+        }
 
-    virtual void OnStep() final {
-        ////! if you want to handle the conflict of multiple-inheritance, you can specify the class before a function call
-        //const SC2APIProtocol::Observation* observation = Agent::Observation()->GetRawObservation();
+        void DrawFeatureLayerHeightMap8BPP(const SC2APIProtocol::ImageData & image_data, int off_x, int off_y) {
+            assert(image_data.bits_per_pixel() == 8);
+            int width = image_data.size().x();
+            int height = image_data.size().y();
+            sc2::renderer::Matrix8BPPHeightMap(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
+        }
 
-        //const SC2APIProtocol::FeatureLayers& m = observation->feature_layer_data().renders();
-        ///*DrawFeatureLayerUnits8BPP(m.unit_density(), 0, 0);*/
+        const float kCameraWidth = 24.0f;
+        const int kFeatureLayerSize = 80;
+        const int kPixelDrawSize = 5;
+        const int kDrawSize = kFeatureLayerSize * kPixelDrawSize;
 
-        //renderer::Render();
-    }
-
-
-private:
-    //void DrawFeatureLayerUnits8BPP(const SC2APIProtocol::ImageData& image_data, int off_x, int off_y) {
-    //    assert(image_data.bits_per_pixel() == 8);
-    //    int width = image_data.size().x();
-    //    int height = image_data.size().y();
-    //    renderer::Matrix8BPPPlayers(image_data.data().c_str(), width, height, off_x, off_y, kPixelDrawSize, kPixelDrawSize);
-    //}
-
-    //const float kCameraWidth = 24.0f;
-    const int kFeatureLayerSize = 80;
-    const int kPixelDrawSize = 5;
-    const int kDrawSize = kFeatureLayerSize * kPixelDrawSize;
-
-};
+    };
 
 
+}
 
 #endif // !REMOTE_DRAW_H
--- src/sc2renderer/sc2_renderer.cc
+++ src/sc2renderer/sc2_renderer.cc
@@ -9,6 +9,7 @@ namespace {
     SDL_Window* window_;
     SDL_Renderer* renderer_;
 
+    //? Liu: Create a SDL_Rect, maybe there isn't a rect creator in SDL
     SDL_Rect CreateRect(int x, int y, int w, int h) {
         SDL_Rect r;
         r.x = x;
@@ -95,31 +96,38 @@ void Matrix8BPPPlayers(const char* bytes, int w_mat, int h_mat, int off_x, int o
     assert(renderer_);
     assert(window_);
 
+    //? Liu: prepare a big "pixel" to be drawn
     SDL_Rect rect = CreateRect(0, 0, px_w, px_h);
+    //? Liu: based on the input data's properties, it draws the individuals in this data
     for (size_t y = 0; y < h_mat; ++y) {
         for (size_t x = 0; x < w_mat; ++x) {
+            //? Liu: Relocate the "pixel"
             rect.x = off_x + (int(x) * rect.w);
             rect.y = off_y + (int(y) * rect.h);
 
+            //? Liu: coordinator - index conversion
             size_t index = x + y * w_mat;
             switch (bytes[index]) {
             case 0:
-                SDL_SetRenderDrawColor(renderer_, 0, 0, 0, 255);
+                //? Liu: renderer+RGBA, in Alpha Channel, white(255) represents opacity, black(0) represent transparency
+                SDL_SetRenderDrawColor(renderer_, 0, 0, 0, 255);//? Liu: black background
                 break;
             case 1:
                 // Self.
-                SDL_SetRenderDrawColor(renderer_, 0, 255, 0, 255);
+                SDL_SetRenderDrawColor(renderer_, 0, 255, 0, 255);//? Liu: Green
                 break;
             case 2:
                 // Enemy.
-                SDL_SetRenderDrawColor(renderer_, 255, 0, 0, 255);
+                //? Liu: Ally
+                SDL_SetRenderDrawColor(renderer_, 255, 0, 0, 255);//? Liu: This is not enemy, this is Ally
                 break;
             case 3:
                 // Neutral.
                 SDL_SetRenderDrawColor(renderer_, 0, 0, 255, 255);
                 break;
             case 4:
-                SDL_SetRenderDrawColor(renderer_, 255, 255, 0, 255);
+                //? Liu: Enemy
+                SDL_SetRenderDrawColor(renderer_, 255, 255, 0, 255);//? Liu: this is enemy, yellow(R+G=Y)
                 break;
             case 5:
                 SDL_SetRenderDrawColor(renderer_, 0, 255, 255, 255);
