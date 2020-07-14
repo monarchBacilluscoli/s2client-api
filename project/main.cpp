372310a536d3baabcac5627731996b63336a8bcb
LiuYongfeng <531817981@qq.com>
2020-07-14 20:22:10 +0800

    + tested GetTeamHealth()

--- project/main.cpp
+++ project/main.cpp
@@ -47,31 +47,136 @@ public:
     }
 };
 
+class ANBot : public Agent
+{
+    void OnGameEnd() final
+    {
+        AgentControl()->Restart();
+    }
+
+    void OnUnitIdle(const Unit *u) final
+    {
+        Units enemies = Observation()->GetUnits(Unit::Alliance::Enemy);
+        Actions()->UnitCommand(u, ABILITY_ID::ATTACK, enemies.front());
+    }
+};
+
 int main(int argc, char *argv[])
 {
-    // {
-    //     // 测试直观可行性
-    //     Simulator sim(2);
-    //     sim.LoadSettings(argc, argv);
-    //     std::string starcraft_path = sim.GetExePath();
-    //     sim.SetProcessPath(starcraft_path);
-    //     sim.SetPortStart(2000);
-    //     sim.SetMapPath("2P_EnemyZealotModVSMarinesSim.SC2Map");
-    //     sim.SetStepSize(1);
-    //     sim.LaunchStarcraft();
-    //     sim.StartGame();
-    //     for (size_t i = 0; i < 100; i++)
-    //     {
-    //         sim.Update();
-    //     }
-    //     sim.Control()->SaveReplay("2psimulator_test.SC2Replay");
-    //     //? 结论，多人游戏可以debug
-    //     //todo 输入指令观察数据
-    //     //todo 创造一系列攻击指令给敌我单位（有无“获得”？默认或为有？）
-    //     //todo 我方站成一列，敌方zealot依序砍一刀。
-    //     //todo 注意录像。
-    //     return 0;
-    // }
+    {
+        {
+            ANBot anbot;
+            //todo 输入指令观察数据
+            Coordinator coordinator;
+            coordinator.LoadSettings(argc, argv);
+            std::string starcraft_path = coordinator.GetExePath();
+            coordinator.SetProcessPath(starcraft_path);
+            std::string map_path = coordinator.GetMapPath(); // you can set your own map_path here
+            coordinator.SetParticipants({CreateParticipant(Race::Terran, &anbot),
+                                         CreateComputer(Race::Terran)});
+            coordinator.LaunchStarcraft();
+            coordinator.StartGame();
+
+            Simulator sim(2);
+            sim.LoadSettings(argc, argv);
+            sim.SetProcessPath(starcraft_path);
+            sim.SetPortStart(2000);
+            sim.SetMapPath("2P_EnemyZealotModVSMarinesSim.SC2Map");
+            sim.SetStepSize(1);
+            sim.LaunchStarcraft();
+            sim.StartGame();
+
+            // sim.Debug(1)->DebugKillUnits(sim.Observation()->GetUnits());
+            for (int i = 0; i < 10; i++)
+            {
+                coordinator.Update();
+            }
+
+            sim.Actions()->SendChat("start copy");
+            sim.CopyAndSetState(anbot.Observation());
+            sim.Actions()->SendChat("end copy, start dry update");
+            for (int i = 0; i < 100; i++)
+            {
+                sim.Update();
+            }
+            sim.Actions()->SendChat("end dry update");
+            // 构建命令
+            Units my_units = anbot.Observation()->GetUnits(Unit::Alliance::Self);
+            Units enemies = anbot.Observation()->GetUnits(Unit::Alliance::Enemy);
+            std::vector<Command> commands(my_units.size());
+            std::vector<Command> enemy_commands(enemies.size());
+            // 构建我方
+            for (size_t i = 0; i < my_units.size(); i++)
+            {
+                Command c; // many actions
+                c.unit_tag = my_units[i]->tag;
+                ActionRaw a;
+                a.ability_id = ABILITY_ID::MOVE;
+                a.target_type = ActionRaw::TargetPosition;
+                a.target_point = Point2D(16, 13 + 3 * i);
+                for (size_t i = 0; i < 3; i++)
+                {
+                    c.actions.push_back(a);
+                }
+                commands[i] = c;
+            }
+            // 构建敌方
+            for (size_t i = 0; i < enemies.size(); i++)
+            {
+                Command c;
+                c.unit_tag = enemies[i]->tag;
+                ActionRaw a;
+                a.ability_id = ABILITY_ID::ATTACK;
+                a.target_type = ActionRaw::TargetPosition;
+                for (size_t j = 0; j < my_units.size(); j++) // 分别攻击几个我方单位
+                {
+                    a.target_point = Point2D(16, 13 + 3 * j);
+                    c.actions.push_back(a);
+                }
+                a.target_point = Point2D(27, 16); // 远离我方单位
+                c.actions.push_back(a);
+                enemy_commands[i] = c;
+            }
+
+            sim.Actions()->SendChat("start copy state and command");
+            sim.SetStartPoint(commands, enemy_commands, anbot.Observation());
+            sim.Actions()->SendChat("end copy, start wet update");
+            for (int i = 0; i < 300; i++)
+            {
+                sim.Update();
+            }
+            sim.Actions()->SendChat("player 1 loss: " + std::to_string(sim.GetTeamHealthLoss(1)));
+            sim.Actions()->SendChat("player 2 loss: " + std::to_string(sim.GetTeamHealthLoss(2)));
+            sim.Actions()->SendChat("end wet update, start dry update");
+            for (int i = 0; i < 100; i++)
+            {
+                sim.Update();
+            }
+            sim.Control()->SaveReplay("copy_and_set_state_test.SC2Replay");
+
+            return 0;
+        }
+        {
+            // 测试直观可行性
+            Simulator sim(2);
+            sim.LoadSettings(argc, argv);
+            std::string starcraft_path = sim.GetExePath();
+            sim.SetProcessPath(starcraft_path);
+            sim.SetPortStart(2000);
+            sim.SetMapPath("2P_EnemyZealotModVSMarinesSim.SC2Map");
+            sim.SetStepSize(1);
+            sim.LaunchStarcraft();
+            sim.StartGame();
+            for (size_t i = 0; i < 100; i++)
+            {
+                sim.Update();
+            }
+            sim.Control()->SaveReplay("2psimulator_test.SC2Replay");
+            //? 结论，多人游戏可以debug
+        }
+
+        return 0;
+    }
 
     while (true)
     {
--- project/rolling_bot/simulator/command.h
+++ project/rolling_bot/simulator/command.h
@@ -14,39 +14,39 @@
 
 namespace sc2
 {
-struct Command
-{
-    Tag unit_tag = 0;
-    RawActions actions = RawActions();
-
-    Command() = default;
-
-    bool operator==(const Command &rhs) const
+    struct Command
     {
-        return unit_tag == rhs.unit_tag && actions == rhs.actions;
+        Tag unit_tag = 0;
+        RawActions actions = RawActions();
+
+        Command() = default;
+
+        bool operator==(const Command &rhs) const
+        {
+            return unit_tag == rhs.unit_tag && actions == rhs.actions;
+        };
+        bool operator!=(const Command &rhs) const { return !(*this == rhs); };
+
+        Command(const Command &rhs) : unit_tag(rhs.unit_tag), actions(rhs.actions) {}
+
+        Command &operator=(const Command &rhs)
+        {
+            unit_tag = rhs.unit_tag;
+            actions.assign(rhs.actions.begin(), rhs.actions.end());
+            return *this;
+        }
+
+        size_t size()
+        {
+            return actions.size();
+        }
+        ActionRaw &operator[](size_t index)
+        {
+            return actions[index];
+        }
+
+        static std::map<Tag, std::deque<ActionRaw>> ConmmandsVecToDeque(const std::vector<Command> &commands); // 将算法返回的动作vector转换为deque
     };
-    bool operator!=(const Command &rhs) const { return !(*this == rhs); };
-
-    Command(const Command &rhs) : unit_tag(rhs.unit_tag), actions(rhs.actions) {}
-
-    Command &operator=(const Command &rhs)
-    {
-        unit_tag = rhs.unit_tag;
-        actions.assign(rhs.actions.begin(), rhs.actions.end());
-        return *this;
-    }
-
-    size_t size()
-    {
-        return actions.size();
-    }
-    ActionRaw &operator[](size_t index)
-    {
-        return actions[index];
-    }
-
-    static std::map<Tag, std::deque<ActionRaw>> ConmmandsVecToDeque(const std::vector<Command> &commands); // 将算法返回的动作vector转换为deque
-};
 
 } // namespace sc2
 
